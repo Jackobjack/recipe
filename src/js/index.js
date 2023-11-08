b0a62bb082110d2308c187f16c78c018d78edea9
Jackobjack <jackobjack@yahoo.de>
2023-11-08 19:17:04 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,44 @@
 // require ("@babel/polyfill");
-// fetch(url); 
-import axios from 'axios';
 import Search from './model/search';
 
-// Класс обьет үүсгэв
-let search = new Search('pasta');
+/** Web app төлөв
+* - Хайлтын query, үр дүн
+* - Тухайн үзүүлж байгаа жор
+* - Лайкалсан жорууд
+* - Захиалж байгаа жорын найрлагууд
+*/
+const state = {};
 
-// Түүн доторх функцийг дуудав
-search.doSearch().then(r => console.log(r));
+const controlSearch = async () => {
+    // 1. Вебээс хайлтын түлхүүр үгийг гаргаж авна
+    const query = 'pizza';
+    // query = document.querySelector('.search__field').value
+
+    // 2. Шинээр хайлтын обьектыг үүсгэж өгнө
+    if (query){
+        state.search = new Search(query);
+        
+        // 3. Хайлт хийхэд зориулж дэлгэцийн UI бэлтгэж өгнө.
+
+    
+        // 4. Хайлтыг гүйцэтгэнэ
+        await state.search.doSearch();
+    
+        // 5. Хайлтын үр дүнг гүйцэтгэнэ
+        console.log(state.search.result);
+        // const html = '';
+        // document.querySelector('.').classList.add('beforeend', html);  
+    }
+    
 
 
+    
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
 
 
 
